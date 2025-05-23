261d7d08c2688cf50fb45436198554b2acd6a96d
brissss <148284979+brissss@users.noreply.github.com>
2025-05-23 14:07:59 -0600

    ej2
    Se implemento la promesa para poder verificar si es admin o no

--- Practica3/main.js
+++ Practica3/main.js
@@ -1,2 +1,6 @@
 import restar from './utils.js';
 console.log(restar(5, 3));
+
+verificarUsuario("admin")
+  .then(mensaje => console.log(mensaje))
+  .catch(error => console.log(error));
--- Practica3/utils.js
+++ Practica3/utils.js
@@ -1,3 +1,13 @@
 export default function restar (a, b) {
     return a - b;
 }
+
+export function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+      if (usuario === "admin") {
+        resolve("Acceso concedido");
+      } else {
+        reject("Acceso denegado");
+      }
+    });
+  }
